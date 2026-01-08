844003bd43aace04ec9eb34cf76946c67141cfcd
Solomon <51864059+MaxBotCoder@users.noreply.github.com>
2026-01-07 21:59:20 -0800

    Update exigence-GPU-driver-manager.py

--- exigence-GPU-driver-manager.py
+++ exigence-GPU-driver-manager.py
@@ -1,10 +1,28 @@
 #Startup message
-print("Welcome to Exegence-OS-executable-installer version 0.1.0 alpha")
+print("Welcome to Exegence-OS-GPU-driver-installer version 0.1.0 alpha")
 
 #modules/libraries
+import os
 
 #variables
+commands = ""
 
 #functions
+def install_nvidia_gpu_driver():
+    print("installing nvidia gpu driver")
+
+def install_intel_gpu_driver():
+    print("installing intel gpu driver")
+
+def install_amd_gpu_driver():
+    print("installing amd gpu driver")
+
+def detect_GPU():
+    print("detecting gpu")
+
+def command_input(command_input):
+    if commands 
 
 #call functions
+while commands != "quit" and commands != "exit":
+    command_input(commands)
